a5063d3d4c6eb4db238bd3ae528e1772c431c7a7
hyesung oh <haesungoh414@gmail.com>
2021-05-22 03:21:45 +0900

    feat: styling Post page's writer section

--- frontend/src/components/Post/WriterSection.tsx
+++ frontend/src/components/Post/WriterSection.tsx
@@ -6,7 +6,31 @@ interface WriterSectionProps {
 }
 
 const WriterSection = ({ writer }: WriterSectionProps) => {
-    return <StyledSection>{writer.email}</StyledSection>;
+    return (
+        <StyledSection>
+            <ProfileImageDiv>
+                <ProfileImage
+                    src={writer.userImageUrl}
+                    alt={`${writer.name} profile image`}
+                />
+            </ProfileImageDiv>
+            <ProfileInfoDiv>
+                <BoldSpan>{writer.name}</BoldSpan>
+                <SoftSpan>{writer.email}</SoftSpan>
+
+                <ProfileYearDiv>
+                    <ProfileSeparatedDiv>
+                        <BoldSpan>입학 년도</BoldSpan>
+                        <SoftSpan>{writer.entranceYear}</SoftSpan>
+                    </ProfileSeparatedDiv>
+                    <ProfileSeparatedDiv>
+                        <BoldSpan>졸업 년도</BoldSpan>
+                        <SoftSpan>{writer.graduationYear}</SoftSpan>
+                    </ProfileSeparatedDiv>
+                </ProfileYearDiv>
+            </ProfileInfoDiv>
+        </StyledSection>
+    );
 };
 
 export default WriterSection;
@@ -14,10 +38,62 @@ export default WriterSection;
 const StyledSection = styled.section`
     position: fixed;
     left: calc(50vw + 300px + 12px);
-    top: 80px;
+    top: 90px;
 
     width: 300px;
-    height: 200px;
+    padding: 10px 10px;
     background-color: ${({ theme }) => theme.colors.white};
     border-radius: ${({ theme }) => theme.border_intensity};
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const ProfileImageDiv = styled.div`
+    position: relative;
+    overflow: hidden;
+    
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: red;
+    margin-bottom: 8px;
+`;
+
+const ProfileImage = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
+
+const ProfileInfoDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const BoldSpan = styled.span`
+    font-size: 1rem;
+`;
+
+const SoftSpan = styled.span`
+    font-size: 0.8rem;
+    opacity: 0.5;
+`;
+
+const ProfileYearDiv = styled.div`
+    width: 100%;
+    display: flex;
+    margin-top: 8px;
+`;
+
+const ProfileSeparatedDiv = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
 `;
