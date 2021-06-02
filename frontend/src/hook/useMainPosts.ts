82c8f827042426721371347cbbb42b888aad9764
hyesung oh <haesungoh414@gmail.com>
2021-06-03 00:45:27 +0900

    feat: add basic intersectionObserver to homeContainer

--- frontend/src/hook/useMainPosts.ts
+++ frontend/src/hook/useMainPosts.ts
@@ -1,6 +1,11 @@
+import { baseUrl } from "api/url";
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const useMainPosts = () => {};
+const useMainPosts = () => {
+    // const {} = useQuery("mainPosts", axios.get(`${baseUrl}/`))
+
+    return {};
+};
 
 export default useMainPosts;
--- frontend/src/pages/home/container/HomeContainer.tsx
+++ frontend/src/pages/home/container/HomeContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import HomePresenter from "../presenter/HomePresenter";
 
 import { RouteComponentProps } from "react-router";
@@ -6,10 +6,27 @@ import { RouteComponentProps } from "react-router";
 import "pages/home/Home.scss";
 
 const HomeContainer = ({ history }: RouteComponentProps) => {
+    const loader = useRef<HTMLDivElement | null>(null);
 
+    const handleObserver = useCallback((entries) => {
+        console.log(entries);
+    }, []);
     
+    useEffect(() => {
+        const option = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 0
+        };
+
+        const observer = new IntersectionObserver(handleObserver, option);
+        if (loader.current) observer.observe(loader.current);
+    })
     // return <HomePresenter loading={loading} posts={posts} />;
-    return <div></div>;
+    return <div>
+        
+        <div ref={loader}></div>
+    </div>;
 };
 
 export default HomeContainer;
