cfb9697da21c16dc03a16f55382d40bb03879cf7
hyesung oh <haesungoh414@gmail.com>
2021-06-03 23:52:14 +0900

    fix: delete intersectionObserver in HomeContainer

--- frontend/src/pages/home/container/HomeContainer.tsx
+++ frontend/src/pages/home/container/HomeContainer.tsx
@@ -1,32 +1,11 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
 import HomePresenter from "../presenter/HomePresenter";
-
 import { RouteComponentProps } from "react-router";
-
 import "pages/home/Home.scss";
 
 const HomeContainer = ({ history }: RouteComponentProps) => {
-    const loader = useRef<HTMLDivElement | null>(null);
-
-    const handleObserver = useCallback((entries) => {
-        console.log(entries);
-    }, []);
-    
-    useEffect(() => {
-        const option = {
-            root: null,
-            rootMargin: "20px",
-            threshold: 0
-        };
-
-        const observer = new IntersectionObserver(handleObserver, option);
-        if (loader.current) observer.observe(loader.current);
-    })
     // return <HomePresenter loading={loading} posts={posts} />;
-    return <div>
-        
-        <div ref={loader}></div>
-    </div>;
+
+    return <div></div>;
 };
 
 export default HomeContainer;
