179613ae7cf99b95de37421dbdcd1e43f9803777
hyesung oh <haesungoh414@gmail.com>
2021-06-08 03:57:24 +0900

    feat: apply useSuggestion for post suggestion

--- frontend/src/hook/useSuggestion.ts
+++ frontend/src/hook/useSuggestion.ts
@@ -1,10 +1,29 @@
+import { baseUrl } from "api/url";
 import axios from "axios";
-
+import { useMutation } from "react-query";
+import { useRecoilState } from "recoil";
+import { suggestionState } from "store/suggestion";
 
 const useSuggestion = () => {
+    const [suggestion, setSuggestion] = useRecoilState(suggestionState);
 
+    const refresh = () => {
+        setSuggestion({
+            title: "",
+            content: "",
+        });
+    };
+    const createSuggestion = useMutation(
+        () => {
+            return axios.post(`${baseUrl}/api/suggestions`, {
+                content: suggestion.content,
+                title: suggestion.title,
+            });
+        },
+        { onSuccess: () => refresh() }
+    );
 
-    return {};
+    return { createSuggestion: createSuggestion.mutate };
 };
 
 export default useSuggestion;
