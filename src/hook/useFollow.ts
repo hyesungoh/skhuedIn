bc9e9978e6421ae5857566fe21711cac995b3cce
hyesung oh <haesungoh414@gmail.com>
2021-07-04 21:12:43 +0900

    feat: add function to useFollow custom hook

--- src/hook/useFollow.ts
+++ src/hook/useFollow.ts
@@ -1,3 +1,18 @@
-const useFollow = () => {};
+import { useRecoilValue } from "recoil";
+
+import { currentUserState } from "store/user";
+
+const useFollow = () => {
+    const currentUser = useRecoilValue(currentUserState);
+
+    const followUserById = () => {};
+
+    const unfollowUserById = () => {};
+
+    return {
+        followUserById,
+        unfollowUserById,
+    };
+};
 
 export default useFollow;
