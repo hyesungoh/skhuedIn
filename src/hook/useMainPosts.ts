85c7e801da7446532f5295971bf3d127424fc124
hyesung oh <haesungoh414@gmail.com>
2021-07-25 19:07:14 +0900

    update: add comment to useMainPosts custom hook

--- src/hook/useMainPosts.ts
+++ src/hook/useMainPosts.ts
@@ -1,22 +1,26 @@
 import axios, { AxiosResponse } from "axios";
 import { useQuery } from "react-query";
 import { useRecoilState, useSetRecoilState } from "recoil";
+
+import { baseUrl } from "api/url";
+import { IGetMainPosts } from "types/fetch";
+import { IPost } from "types";
 import {
     hasMorePostsState,
     mainPostsPageState,
     mainPostsState,
 } from "store/posts";
 
-import { baseUrl } from "api/url";
-import { IGetMainPosts } from "types/fetch";
-import { IPost } from "types";
-
 const useMainPosts = () => {
+    // api 호출 시에 page값을 증가하여 관리하여 state화 하였습니다.
     const [mainPostsPage, setMainPostsPage] =
         useRecoilState(mainPostsPageState);
+
     const [mainPosts, setMainPosts] = useRecoilState(mainPostsState);
     const setHasMorePosts = useSetRecoilState(hasMorePostsState);
 
+    // response를 post로 이루어진 data로 반환하며,
+    // 전부 비었을 때, hasMorePosts를 true화 합니다.
     const getDataToPosts = (data: AxiosResponse<IGetMainPosts>) => {
         let isAllEmpty = false;
         let tempArray: IPost[] = [];
@@ -32,16 +36,20 @@ const useMainPosts = () => {
         return tempArray;
     };
 
+    // global state mainPosts을 response의 data값으로 합니다.
     const setOnMount = (data: AxiosResponse<IGetMainPosts>) => {
         const tempArray = getDataToPosts(data);
         setMainPosts([...tempArray]);
     };
 
+    // global state mainPosts에 response의 data값을 추가 합니다.
     const setOnIntersect = (data: AxiosResponse<IGetMainPosts>) => {
         const tempArray = getDataToPosts(data);
         setMainPosts([...mainPosts, ...tempArray]);
     };
 
+    // mainPostsPage를 0으로 할당 후, ajax 합니다.
+    // 성공했을 시, setOnMount 합니다.
     const { isLoading } = useQuery(
         "mainPosts",
         () => {
@@ -57,6 +65,8 @@ const useMainPosts = () => {
         }
     );
 
+    // 감지됐을 때 실행될 함수로써,
+    // mainPostpage를 증가하고, setOnIntersect를 실행합니다.
     const onIntersect = async () => {
         const data = await axios.get<IGetMainPosts>(
             `${baseUrl}/api/main?page=${mainPostsPage + 1}&size=1`
