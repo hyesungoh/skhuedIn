90b98be62307ca9965a1c5e1926e04c12f35abc6
hyesung oh <haesungoh414@gmail.com>
2021-07-25 18:56:59 +0900

    update: add comment to Newsfeed component

--- src/components/Home/Newsfeed.tsx
+++ src/components/Home/Newsfeed.tsx
@@ -8,17 +8,23 @@ import NewsfeedContent from "components/Home/NewsfeedContent";
 import { hasMorePostsState, mainPostsState } from "store/posts";
 
 const Newsfeed = () => {
+    // infinity scroll을 위해 감지될 요소의 ref
     const observeTarget = useRef<HTMLDivElement>(null);
+    // 현재 렌더링중인 posts에 대한 global state
     const posts = useRecoilValue(mainPostsState);
+    // 서버에 추가적인 post가 있는지에 대한 global state
     const hasMorePosts = useRecoilValue(hasMorePostsState);
 
+    // 로딩 상태와 감지됐을 때 할 행동을 custom hook에서 받아옵니다.
     const { isLoading, onIntersect } = useMainPosts();
 
+    // 포스트의 컨텐츠를 줄바꿈 기준 3줄까지 포매팅합니다.
     const setContentFormat = (ogContent: string) => {
         const fmContent = ogContent.split("\n").slice(0, 3).join("\n");
         return fmContent;
     };
 
+    // custom hook을 이용하여 infinity scroll을 구현합니다.
     useInfinityScroll({
         target: observeTarget.current,
         onIntersect: ([{ isIntersecting }]) => {
