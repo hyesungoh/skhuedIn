313d4d5892c66b2c65fb5efdc0da63b31b4ff08c
hyesung oh <haesungoh414@gmail.com>
2021-08-05 00:20:42 +0900

    refactor: add comment to isGraduate, canMakeBlog

--- src/utils/canMakeBlog.ts
+++ src/utils/canMakeBlog.ts
@@ -1,6 +1,7 @@
 import { ICurrentUser } from "types";
 import isGraduate from "utils/isGraduate";
 
+// 졸업을 했으며 블로그를 생성안한 유저일 때 true를 반환
 const canMakeBlog = (currentUser: ICurrentUser) => {
     const isGraduated = isGraduate(currentUser);
     if (isGraduated && !currentUser.data?.isBlog) return true;
--- src/utils/isGraduate.ts
+++ src/utils/isGraduate.ts
@@ -1,5 +1,6 @@
 import { ICurrentUser } from "types";
 
+// 로그인을 했으며, 졸업을 했을 때 true를 반환
 const isGraduate = (currentUser: ICurrentUser) => {
     if (!currentUser.data) return false;
     if (currentUser.data?.graduationYear !== "0") return true;
