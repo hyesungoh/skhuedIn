8622dc122767efc2663c2cb259af58bcfa5b212e
hyesung oh <haesungoh414@gmail.com>
2021-08-08 20:52:36 +0900

    update: add comment to ScrollToTop component

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,6 +1,8 @@
 import { useEffect, Fragment } from "react";
 import { RouteChildrenProps, withRouter } from "react-router-dom";
 
+
+// router 변경될 시 스크롤 위치를 최상으로
 const ScrollToTop = ({ location }: RouteChildrenProps) => {
     useEffect(() => {
         window.scroll(0, 0);
