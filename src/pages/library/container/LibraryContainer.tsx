4cbf18b4d0d7a013dcf9709c355e8c0c0bcd5616
hyesung oh <haesungoh414@gmail.com>
2021-08-15 16:48:21 +0900

    update: add comment to Library container

--- src/pages/library/container/LibraryContainer.tsx
+++ src/pages/library/container/LibraryContainer.tsx
@@ -5,6 +5,8 @@ import Loading from "components/Loading/Loading";
 import { getBlogsByCategory } from "api/blog/fetch";
 
 const LibraryContainer = () => {
+    // blog 리스트 fetch
+    // custom Hooks로 refactor 필요
     const { data, isLoading, error } = useQuery("blogs", () =>
         getBlogsByCategory("user.lastModifiedDate,ASC")
     );
